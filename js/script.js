2002c52d56a40e25a294aa17b5704536ff4fde50
Zhenya <plaplapla@gmail.com>
2022-09-14 21:14:36 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+ 
+let incr = 10,
+    decr = 10;
+
+  //  ++incr;
+  //  --decr;
+
+    console.log(++incr);
+    console.log(--decr);
+
+    console.log(16%2);
+
+    console.log(2 + 2 * 2 !== 8);
+
+  const isChecked = false,
+  isClose = true;
+
+  console.log(isChecked || !isClose);
